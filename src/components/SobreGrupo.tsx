9e522e82777b6fb38b0059e72807dbab8739e33c
saudemaisfit-beep <saudemaisfit@gmail.com>
2026-06-24 16:32:59 +0100

    feat: replace Centro Cultural image with Hospital Militar in SobreGrupo gallery

--- src/components/SobreGrupo.tsx
+++ src/components/SobreGrupo.tsx
@@ -5,7 +5,7 @@ import { useEffect, useRef, useState } from 'react';
 const IMAGES = [
   { src: '/EN-230-omatapalo-2.jpg',              label: 'Estrada Nacional 230 - Saurimo'   },
   { src: '/Salao-Protocolar-1-1.jpg',            label: 'Salão Protocolar - Luanda'             },
-  { src: '/centro-cultural-huambo-omatapalo.png',label: 'Centro Cultural Manuel Rui - Huambo'   },
+  { src: '/HOSPITAL MILITAR_1306202.JPG',         label: 'Hospital Militar'                        },
   { src: '/omatapalo-construcao-do-monumento-do-soldado-desconhecido.jpg', label: 'Monumento ao Soldado Desconhecido - Luanda' },
   { src: '/DSC_0030.jpg',                        label: 'Missão Fazer Sorrir'             },
   { src: '/Academia-barra.jpg',                  label: 'Academia Omatapalo'           },
